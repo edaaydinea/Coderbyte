376043ee5d864d6f931126e03123f7dbd1067767
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-17 19:20:02 +0300

    Update 2 - Fibonacci Checker.py

--- Patika.dev Coderbyte Assessments (Weekly)/Week 7/Python/2 - Fibonacci Checker.py
+++ Patika.dev Coderbyte Assessments (Weekly)/Week 7/Python/2 - Fibonacci Checker.py
@@ -1,16 +1,16 @@
 def FibonacciChecker(num):
+    # code goes here
+    fn = fn_1 = 0
+    fn_2 = 1
 
-  # code goes here
-  fn = fn_1 = 0
-  fn_2 = 1
+    while fn <= num:
+        if fn == num:
+            return "yes"
+        fn = fn_1 + fn_2
+        fn_2 = fn_1
+        fn_1 = fn
+    return "no"
 
-  while fn <= num:
-    if fn == num:
-      return "yes"
-    fn = fn_1 + fn_2
-    fn_2 = fn_1
-    fn_1 = fn
-  return "no"
 
 # keep this function call here
 print(FibonacciChecker(input()))
