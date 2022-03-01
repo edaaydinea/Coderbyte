aaa103657598a1c80328741e96dcf4e1a8c779f0
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-01 19:29:12 +0300

    Update Find Insersection.py

--- Easy/Find Insersection.py
+++ Easy/Find Insersection.py
@@ -1,10 +1,10 @@
 def FindIntersection(strArr):
-    # code goes here
-    a = map(int, strArr[0].split(", "))
-    b = map(int, strArr[1].split(", "))
-    c = list(set(a) & set(b))
-    c.sort()
-    return ",".join(map(str, c))
+    setOne = set(strArr[0].split(", "))
+    setTwo = set(strArr[1].split(", "))
+
+    result = sorted(list(setOne.intersection(setTwo)), key=lambda str: int(str))
+
+    return ','.join(result) if len(result) > 0 else False
 
 
 # keep this function call here
